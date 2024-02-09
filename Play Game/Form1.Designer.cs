b8dcd06473514dcafeefd559d47210105a69117c
aditmish409 <aditmish409@SDS-L-D9HDF63>
2024-02-09 14:05:07 -0500

    Add project files.

---
+++ Play Game/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Play_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startbutton = new System.Windows.Forms.Button();
+            this.readylabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startbutton
+            // 
+            this.startbutton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.startbutton.Font = new System.Drawing.Font("Mongolian Baiti", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startbutton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.startbutton.Location = new System.Drawing.Point(305, 236);
+            this.startbutton.Name = "startbutton";
+            this.startbutton.Size = new System.Drawing.Size(175, 53);
+            this.startbutton.TabIndex = 0;
+            this.startbutton.Text = "Start";
+            this.startbutton.UseVisualStyleBackColor = false;
+            this.startbutton.Click += new System.EventHandler(this.startbutton_Click);
+            // 
+            // readylabel
+            // 
+            this.readylabel.Font = new System.Drawing.Font("Modern No. 20", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.readylabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.readylabel.Location = new System.Drawing.Point(305, 132);
+            this.readylabel.Name = "readylabel";
+            this.readylabel.Size = new System.Drawing.Size(175, 38);
+            this.readylabel.TabIndex = 1;
+            this.readylabel.Text = "Ready?";
+            this.readylabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.readylabel.Click += new System.EventHandler(this.readylabel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.readylabel);
+            this.Controls.Add(this.startbutton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startbutton;
+        private System.Windows.Forms.Label readylabel;
+    }
+}
+
---
+++ Play Game/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace Play_Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void readylabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startbutton_Click(object sender, EventArgs e)
+        {
+
+            //
+            readylabel.Text = "3";
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            readylabel.Text = "2";
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            readylabel.Text = "1";
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            readylabel.Text = "Go!";
+            this.BackColor = Color.Green;
+
+            startbutton.Visible = false;
+
+            
+
+                    }
+    }
+}
